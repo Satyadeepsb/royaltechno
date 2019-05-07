a9226ee382fa9f5fd717d462201c93bb933c8663
Satyadeepsb <satyadeepsb@gmail.com>
2019-05-07 12:07:52 +0530

    index.js change

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-
-window.addEventListener('load', () => {
-  registerSW();
-});
-
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
       await navigator.serviceWorker.register('./sw.js');
     } catch (e) {
-      console.log(`SW registration failed`);
+      console.log('SW registration failed');
     }
   }
 }
+
+
+window.addEventListener('load', () => {
+  registerSW();
+});
+
